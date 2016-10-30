c78bcfb55d6ed0a1535d8e4254559400d417693e
覃庆显 <971252914@qq.com>
2016-10-30 15:47:51 +0800

    增加对callback函数的注释

--- js/linkConvert.js
+++ js/linkConvert.js
@@ -2,8 +2,10 @@ var re = /^http:\/\/live.bilibili.com/;
 
 if(re.exec(window.location.href) != null){
     var roomid = window.location.href.match(/\d+/g);
+    //这个function名与下面的js标签的callback=?对应,若脚本执行成功,result的值就会变成src中传回来的JSON
     function jsonpCallback(result) {
-        console.log(result);
+        // 测试是否取到JSON
+        // console.log(result);
         console.log(result['durl'][0]['url']);
         var url = result['durl'][0]['url'];
         window.open(url);
