c25f68c490759c3dc38a928c9b0944ff248de5e1
覃庆显 <971252914@qq.com>
2016-10-31 01:36:19 +0800

    增加jsonp:callback

--- js/m3u8Get.js
+++ js/m3u8Get.js
@@ -1,12 +1,14 @@
-function m3u8Get(roomID){
-var outData = $.ajax({
-    url: 'http://live.bilibili.com/api/h5playurl',
-    type: 'get',
-    dataType: 'jsonp',
-    data:{
-        roomid: roomID
-    },
-    jsonpCallback : "miaomiaomiao",
-});
-return outData
+function m3u8Get(roomID) {
+    var outData = $.ajax({
+        url: 'http://live.bilibili.com/api/h5playurl',
+        type: 'get',
+        dataType: 'jsonp',
+        jsonp: 'callback',
+        jsonpCallback: "miaomiaomiao",
+        data: {
+            roomid: roomID
+        },
+
+    });
+    return outData
 }
