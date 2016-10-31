0bec456d57ed5a64c5eb5811cf3afeecb3666174
LASER-Yi <ly0007@yeah.net>
2016-10-31 21:16:18 +0800

    追加注释

--- js/jqInject.js
+++ js/jqInject.js
@@ -24,7 +24,7 @@ if(re.exec(window.location.href) != null){
 /*var divElements = document.getElementsById("player-container");
  var onclick_script = document.creatElement('script');*/
 
-//替换上的代码
+//替换上的代码（位于播放器节点）
 
 /*<div id="player-container" class="player-ctnr p-relative main-section">
 <video src = h5Url width="100%" height="100%" id="player_object" controls="controls" autoplay="autoplay"></video>
@@ -32,6 +32,7 @@ if(re.exec(window.location.href) != null){
 
 //h5Url这个变量要加到头部的变量库中，然后通过ajax给他赋值
 
-//宝箱节点需要整个屏蔽，不然无法控制视频（叠层原因）
+//宝箱节点需要整个屏蔽，不然无法控制视频（叠层原因）（可能不用屏蔽，当时的网络加载不出来，估计就覆盖上去了。
+//宝箱节点 class="box-doms"
 
 
